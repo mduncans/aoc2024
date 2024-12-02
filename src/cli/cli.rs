da004b5d575d293e9835dfe0f4f0a26ccc5de198
Matt Smith <msmith21794@gmail.com>
2024-12-01 21:12:45 -0500

    added command to cli for day selection

---
+++ src/cli/cli.rs
@@ -0,0 +1,24 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about)]
+pub struct Args {
+    // Path to input data
+    #[arg(short, long, help = "Specify the path to the input data file.")]
+    pub input: PathBuf,
+
+    // Problem part - a or b
+    #[arg(short, long, default_value = "a", help = "Specify the problem part ('a' or 'b').")]
+    pub part: String,
+
+    // What day to solve
+    #[command(subcommand)]
+    pub command: Commands
+}
+
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    Day1,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use aoc2024::{cli::args, days};
+use aoc2024::{cli::cli, days};
 use clap::Parser;
 
 fn main() {
-    let args = args::Args::parse();
-    let result = days::day1::solve(args.input, &args.part).unwrap();
+    let args = cli::Args::parse();
+    let command = args.command;
+
+    let result = match command {
+        cli::Commands::Day1 => days::day1::solve(args.input, &args.part).unwrap(),
+    };
 
     println!("{}", result);
 }
